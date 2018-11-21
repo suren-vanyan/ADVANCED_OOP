05b6cb78d4718c8f38172d581fadf314a1abbada
Robert Van <vanyansuren@hotmail.com>
2018-11-21 23:05:40 +0400

    Diferences between class and struct

---
+++ DiffClassAndStruct/DiffClassAndStruct/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DiffClassAndStruct
+{
+    class MyClass
+    {
+        public string chnage;
+    }
+
+    struct MyStruct
+    {
+        public string chnage;
+    }
+    class Program
+    {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.chnage = "Changed";
+        }
+
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.chnage = "Changed";
+        }
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+            myClass.chnage = "Not changed";
+            Console.WriteLine($"MyClass {myClass.chnage}");
+            myStruct.chnage = "Not chnaged";
+            Console.WriteLine($"MyStruct {myStruct.chnage}");
+            ClassTaker(myClass);
+            Console.WriteLine($"after call ClassTaker result is {myClass.chnage}");
+            StructTaker(myStruct);
+            Console.WriteLine($"after call StructTaker result is {myStruct.chnage}");
+        }
+    }
+}
