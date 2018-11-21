fcbcaf1680b4b7bfaaf3a556bcba9bd149c44b57
Robert Van <vanyansuren@hotmail.com>
2018-11-21 15:08:07 +0400

    TrainStation

---
+++ TrainStation(struct)/TrainStation(struct)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainStation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
