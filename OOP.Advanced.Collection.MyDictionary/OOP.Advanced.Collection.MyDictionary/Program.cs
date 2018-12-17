c2b64282d114b0ea9cc23b902440dd2c48d4ebf4
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-18 01:31:43 +0400

    MyDictionary

---
+++ OOP.Advanced.Collection.MyDictionary/OOP.Advanced.Collection.MyDictionary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP.Advanced.Collection.MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
