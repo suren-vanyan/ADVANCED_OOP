ba8a586f623af53b3eb0ed3d944e78c03ee1764a
Suren Vanyan <vanyansuren@hotmail.com>
2018-12-25 00:30:48 +0400

    change collection

--- OOP.Advance.Regex.FindAllNumbersInAnyBrackets/Program.cs
+++ OOP.Advance.Regex.FindAllNumbersInAnyBrackets/Program.cs
@@ -9,7 +9,7 @@ namespace OOP.Advance
         static void Main(string[] args)
         {
            // Find a List of floating point numbers, which occur in any type of brackets.
-            string test = "(10.77)qazwsx(0.5)djsj.a'<6.7>{11.6}sjjjd{4.5}<68.4> ";
+            string test = "(10.77)qazwsx(0.5)djsj.a'<6.7>{11.6}sjjjd[4.5]<68.4> ";
             FindAllFlPointInAnyBrackets(test);
           
         }
@@ -17,21 +17,20 @@ namespace OOP.Advance
         static void FindAllFlPointInAnyBrackets(string input)
         {                              
              
-            string pattern= @"(?<brOne>\(\d+[.]\d+\))|(?<brTwo>\[\d+\\.\d+\])
+            string pattern= @"(?<brOne>\(\d+[.]\d+\))|(?<brTwo>\[\d+\[.]\d+\])
                                 |(?<brThree>\<\d+[.]\d+\>)|(?<brFour>\{\d+[.]\d+\})";
             var collection = Regex.Matches(input, pattern);
             string temp = string.Empty;
             foreach (Match group in collection)
             {
-                ConsoleColor c = (ConsoleColor)(new Random().Next(0, 15));
+                ConsoleColor c = (ConsoleColor)(new Random().Next(1, 15));
                 Console.ForegroundColor = c;
-                temp = group.Value;
-                Console.WriteLine(temp.Replace("()", ""));
-
+                var newMatchCollection = Regex.Matches(group.Value, @"(\d+[.]\d+)");
+                foreach (var item in newMatchCollection)
+                {
+                    Console.WriteLine(item);
+                }               
             }
-
-        }
-
-       
+        }      
     }
 }
