d1767c45eff35cf51a6e79e7702e149aad5df371
suren-vanyan <vanyansuren@hotmail.com>
2018-12-27 00:10:40 +0400

    add comment

--- OOP.Advance.Regex.FindAllNumbersInAnyBrackets/Program.cs
+++ OOP.Advance.Regex.FindAllNumbersInAnyBrackets/Program.cs
@@ -17,20 +17,24 @@ namespace OOP.Advance
         static void FindAllFlPointInAnyBrackets(string input)
         {
             //different syntax but group in the same way
-            string matchedSubexpressions = @"(\([+-]?\d+[.]\d+\))|(\[[+-]?\d+[.]\d+])|(\<[+-]?\d+[.]\d+\>)|(\{[+-]?\d+[.]\d+\})";
+            //string matchedSubexpressions = @"(\([+-]?\d+[.]\d+\))|(\[[+-]?\d+[.]\d+])|(\<[+-]?\d+[.]\d+\>)|(\{[+-]?\d+[.]\d+\})";
             string namematchedSubexpressions = @"(?<brOne>\[[+-]?\d+[.]\d+])|(?<brTwo>\([-+]?\d+[.]\d+\))|(?<brThree>\<[+-]?\d+[.]\d+\>)|(?<brFour>\{[+-]?\d+[.]\d+\})";
-            string namematchedSubexpressions2 = @"(?'br1'\[[+-]?\d+[.]\d+])|(?'br2'\([+-]?\d+[.]\d+\))|(?'br3'\<[+-]?\d+[.]\d+\>)|(?'br4'\{[+-]?\d+[.]\d+\})";
-            var collection = Regex.Matches(input, matchedSubexpressions);
+            //string namematchedSubexpressions2 = @"(?'br1'\[[+-]?\d+[.]\d+])|(?'br2'\([+-]?\d+[.]\d+\))|(?'br3'\<[+-]?\d+[.]\d+\>)|(?'br4'\{[+-]?\d+[.]\d+\})";
+            var collection = Regex.Matches(input, namematchedSubexpressions);
 
             foreach (Match group in collection)
             {
+                
                 Console.ForegroundColor = (ConsoleColor)(new Random().Next(1, 15));
                 var newMatchCollection = Regex.Matches(group.Value, @"(\d+[.]\d+)");
                 foreach (var item in newMatchCollection)
                 {
                     Console.WriteLine(item);
+
                 }
             }
+
+          
         }
     }
 }
