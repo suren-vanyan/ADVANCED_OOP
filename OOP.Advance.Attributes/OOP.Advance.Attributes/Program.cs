c86fc9eb665c72228694f25fe581d32c748ba268
suren-vanyan <vanyansuren@gmail.com>
2019-01-06 23:44:17 +0400

    add folder Attributes

---
+++ OOP.Advance.Attributes/OOP.Advance.Attributes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP.Advance.Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
