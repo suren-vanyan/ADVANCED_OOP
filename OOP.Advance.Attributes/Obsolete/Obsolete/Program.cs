6092ee9beb173946cffffff683ecca721e77833f
suren-vanyan <vanyansuren@gmail.com>
2019-01-07 00:17:58 +0400

    add Obsolete

---
+++ OOP.Advance.Attributes/Obsolete/Obsolete/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Obsolete
+{
+    class MyClass
+    {
+        [Obsolete("Метод устарел")]
+        public void ObsoleteMessage()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        [Obsolete("Метод не используеться", true)]
+        public void ObsoleteError()
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass instance = new MyClass();
+
+            instance.ObsoleteMessage();
+
+            // instance.ObsoleteError();
+        }
+    }
+}
