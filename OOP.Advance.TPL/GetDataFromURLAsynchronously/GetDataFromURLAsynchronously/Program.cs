f0af28dbe300cbbc74ffea1e59c1db7d30da4a88
suren-vanyan <vanyansuren@gmail.com>
2019-01-15 22:40:46 +0400

    methods that get the date

---
+++ OOP.Advance.TPL/GetDataFromURLAsynchronously/GetDataFromURLAsynchronously/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDataFromURLAsynchronously
+{
+    class Program
+    {
+        static string GetDataFromURL(string url)
+        {
+            // Create a request for the URL.   
+            WebRequest request = WebRequest.Create(url);
+            // If required by the server, set the credentials.  
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Return the content.  
+            return responseFromServer;
+        }
+
+        public static string GetDataFromURL2(string urlAddress)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string data = string.Empty;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+                if (response.CharacterSet == null)
+                    readStream = new StreamReader(receiveStream);
+                else
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                data = readStream.ReadToEnd();
+                response.Close();
+                readStream.Close();
+            }
+
+            return data;
+        }
+
+        static void Main(string[] args)
+        {
+            string url = "https://jsonplaceholder.typicode.com/comments";
+            GetDataFromURL(url);
+        }
+
+      
+    }
+}
