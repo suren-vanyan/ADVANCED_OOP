3d7f403ae8162fc7f6881f89d39926e87aee607a
suren-vanyan <vanyansuren@gmail.com>
2019-01-17 12:18:20 +0400

    yield

---
+++ OOP.Advanced.Collection/UserCollections/03.UserCollectionYield/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.UserCollectionYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            var сollection = new UserCollection<Element>();
+
+            
+            сollection[0] = new Element(1, 2);
+            сollection[1] = new Element(3, 4);
+            сollection[2] = new Element(5, 6);
+            сollection[3] = new Element(7, 8);
+
+            
+            foreach (Element element in сollection)
+            {
+                Console.WriteLine("{0}, {1}", element.FieldA, element.FieldB);
+            }
+
+            Console.WriteLine(new string('-', 5));
+
+           
+            foreach (Element element in сollection)
+            {
+                Console.WriteLine("{0}, {1}", element.FieldA, element.FieldB);
+            }
+
+
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP.Advanced.Collection/UserCollections/03.UserCollectionYield/UserCollection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.UserCollectionYield
+{
+    public class UserCollection<T> 
+    {
+        readonly T[] elements = new T[4];
+
+        public T this[int index]
+        {
+            get { return elements[index]; }
+            set { elements[index] = value; }
+        }
+
+        int position = -1;
+
+    
+        //2.IEnumerator--> Reset
+        public void Reset()
+        {
+            position = -1;
+        }
+
+
+        //4.IEnumerable--> GetEnumerator
+       public IEnumerator GetEnumerator()
+        {
+            while (true)
+            {
+                if (position < elements.Length - 1)
+                {
+                    position++;
+                    yield return elements[position];
+                }
+                else
+                {
+                    Reset();
+                    yield  break;
+                }
+            }
+
+            //foreach (var element in elements)
+            //{
+            //    yield return element;
+            //}
+
+            //return elements.GetEnumerator();
+        }
+
+       
+    }
+}
