e78e58886c7154b554c814fadd6b89630004bca9
suren-vanyan <vanyansuren@gmail.com>
2019-01-17 12:33:38 +0400

    add people collection

---
+++ OOP.Advanced.Collection/UserCollections/00.UserCollection/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00.UserCollection
+{
+    // Simple business object.
+    public class Person
+    {
+        public Person(string fName, string lName)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+        }
+
+        public string firstName;
+        public string lastName;
+    }
+}
---
+++ OOP.Advanced.Collection/UserCollections/00.UserCollection/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00.UserCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] peopleArray = new Person[3]
+            {
+              new Person("John", "Smith"),
+              new Person("Jim", "Johnson"),
+              new Person("Sue", "Rabon"),
+            };
+
+            People peopleList = new People(peopleArray);
+            foreach (Person p in peopleList)
+                Console.WriteLine(p.firstName + " " + p.lastName);
+        }
+    }
+}
