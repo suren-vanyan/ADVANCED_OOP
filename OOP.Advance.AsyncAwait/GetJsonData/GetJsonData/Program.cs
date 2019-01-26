1a2d9fdd018778d2728524f16588e6c7d097d603
suren-vanyan <vanyansuren@gmail.com>
2019-01-27 01:16:42 +0400

    change Task

--- OOP.Advance.AsyncAwait/GetJsonData/GetJsonData/Program.cs
+++ OOP.Advance.AsyncAwait/GetJsonData/GetJsonData/Program.cs
@@ -13,13 +13,21 @@ namespace GetJsonData
             Parallel.ForEach(companies, (company) =>
             {
                 Console.WriteLine($"Company Name:{company.Name},Id:{company.Id}\n");
-                
+
             });
         }
         public static async void Run(string url, CancellationToken cToken)
         {
-           var result=  await Task.Run(() => Call.GetDataAsync(url, cToken));
-            List<Company> companies = JsonConvert.DeserializeObject<List<Company>>(result);
+
+            string result = await Task.Run(() => Call.GetDataAsync(url, cToken));
+            List<Company> companies = null;
+
+            await Task.Run(() =>
+            {
+
+                companies = JsonConvert.DeserializeObject<List<Company>>(result);
+            });
+
             Print(companies);
         }
 
@@ -28,17 +36,16 @@ namespace GetJsonData
             string url = "https://www.itjobs.am/api/v1.0/companies";
             CancellationTokenSource tokenSource = new CancellationTokenSource();
             CancellationToken token = tokenSource.Token;
-            Task task = Task.Factory.StartNew(() => Run(url, token));
-            
+
             try
             {
-                task.Wait();
-                Thread.Sleep(30);
-                tokenSource.Cancel();
+                Run(url, token);
+                 Thread.Sleep(30);
+               // tokenSource.Cancel();
             }
             catch (Exception e) { Console.WriteLine(e.Message); }
 
-           Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
