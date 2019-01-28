7945f51b5e1e2542a3948c74460ef41562c10925
suren-vanyan <vanyansuren@gmail.com>
2019-01-29 00:01:32 +0400

    add scroll

--- JobFinderScrapping/JobFinderScrapping/Helper.cs
+++ JobFinderScrapping/JobFinderScrapping/Helper.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,12 +9,64 @@ namespace JobFinderScrapping
 {
     public static class Helper
     {
-        public static List<Company> ScrapForStaffAM(string url)
+        public static void ToScroll(ChromeDriver chromeDriver)
         {
+           
+            for (int i = 0; i < 80; i++)
+            {
+                try
+                {
+                    chromeDriver.ExecuteScript($"window.scrollBy(0,1750);");
+                }
+                catch (Exception e)
+                {
+                    Program.WriteExceptionInFile(e.Message);
+                }
+                Thread.Sleep(1500);
+            }
+        }
+
+        public static void SearchActiveJob(string url)
+        {          
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(url);
+            string job = "//div[@class=\"job-inner job-item-title\"]";
+
+            string job2 = "//div[@class='job-inner job-list-deadline']";
+            HtmlNodeCollection jobItemTitle = doc.DocumentNode.SelectNodes(job);
+            HtmlNodeCollection jobԼistDeadline = doc.DocumentNode.SelectNodes(job2);
+            List<ActiveJobs> activeJobs = new List<ActiveJobs>();
+            for (int i = 0; i < jobItemTitle.Count; i++)
+            {
+                string[] splitText = jobItemTitle[i].InnerText.Split('\n');
+                string[] splitText2 = jobԼistDeadline[i].InnerText.Split('\n');
+                string temp = splitText2[3] + splitText2[4] + splitText2[5];
+                activeJobs.Add(new ActiveJobs { JobName = splitText[1], CompanyName = splitText[2], Data = temp });
+            }
+
+            foreach (var item in activeJobs)
+            {
+                Console.WriteLine($"Company:{item.CompanyName},Data:{item.Data},Job:{item.JobName}");
+            }
+            Console.ReadLine();
+        }
+
+        public static List<Company> ScrapForStaffAM(string url)
+        {
+            //HtmlWeb web = new HtmlWeb();
+           // HtmlDocument doc = web.Load(url);
 
             string className2 = "//div[@class=\"company-action company_inner_right\"]";
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--disable-images");
+            string directory = @"C:\Users\suren\source\repos\HTMLScrapping\HTMLScrapping\bin\Debug\netcoreapp2.1";
+            ChromeDriver chromeDriver = new ChromeDriver(directory,chromeOptions);
+            chromeDriver.Navigate().GoToUrl(url);
+           // ToScroll(chromeDriver);
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(chromeDriver.PageSource);
 
             HtmlNodeCollection nodes = doc.DocumentNode.SelectNodes(className2);
             List<string> compURLList = new List<string>();
@@ -29,49 +82,50 @@ namespace JobFinderScrapping
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Program.WriteExceptionInFile(e);
             }
             
 
             List<Company> allCompanies = new List<Company>();
             foreach (var compURL in compURLList)
             {
-                HtmlDocument htmlDoc = web.Load(compURL);
+                chromeDriver.Navigate().GoToUrl(compURL);
+                // ToScroll(chromeDriver);
+
+                HtmlDocument htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(chromeDriver.PageSource);
+
                 string companyProperties = "//p[@class=\"professional-skills-description\"]";
                 HtmlNodeCollection htmlNodes = htmlDoc.DocumentNode.SelectNodes(companyProperties);
 
                 string companyProp = "//div[@class='col-lg-8 col-md-8 about-text']";
                 HtmlNodeCollection htmlNodesAboutComp = htmlDoc.DocumentNode.SelectNodes(companyProp);
-                string text = htmlNodesAboutComp[0].InnerText.Replace("\n", "");
+                string textAboutComp = htmlNodesAboutComp[0].InnerText.Replace("\n", "");
 
                 string companyName = "//h1[@class=\"text-left\"]";
                 HtmlNodeCollection htmlNodeOfName = htmlDoc.DocumentNode.SelectNodes(companyName);
 
-                Company company_1 = new Company();
+                Company company = new Company();
 
                
                 try
                 {
-                    company_1.Name = htmlNodeOfName[0].InnerText;
-                    company_1.Industry = htmlNodes[0].InnerText;
-                    company_1.Type = htmlNodes[1].InnerText;
-                    company_1.NumbOfEmployees = htmlNodes[2].InnerText;
-                    company_1.DataOfFoundation = htmlNodes[3].InnerText;
-                    company_1.WebSite = htmlNodes[4].InnerText;
-                    company_1.Adress = htmlNodes[5].InnerText;
-                    company_1.AboutCompany = text;
+                    company.Name = htmlNodeOfName[0].InnerText;
+                    company.Industry = htmlNodes[0].InnerText;
+                    company.Type = htmlNodes[1].InnerText;
+                    company.NumbOfEmployees = htmlNodes[2].InnerText;
+                    company.DataOfFoundation = htmlNodes[3].InnerText;
+                    company.WebSite = htmlNodes[4].InnerText;
+                    company.Adress = htmlNodes[5].InnerText;
+                    company.AboutCompany = textAboutComp;
                 }
-                catch (ArgumentOutOfRangeException e)
+                catch (ArgumentException arg)
                 {
-                    Console.WriteLine(e.Message);
+                    Program.WriteExceptionInFile(arg);
                 }
-
-                
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"This is main information about {company_1.Name}.");
-                Console.ForegroundColor = ConsoleColor.Gray;
-                company_1.DescribeYourself();
-                allCompanies.Add(company_1);
+           
+                allCompanies.Add(company);
+                company.DescribeYourself();
                 Thread.Sleep(4000);
                 Console.Clear();
 
--- JobFinderScrapping/JobFinderScrapping/Program.cs
+++ JobFinderScrapping/JobFinderScrapping/Program.cs
@@ -15,61 +15,32 @@ namespace JobFinderScrapping
 {
     class Program
     {
-        public static void SearchActiveJob()
-        {
-            string url = "https://staff.am/en/jobs?JobsFilter%5Bkey_word%5D=&JobsFilter%5Bcategory%5D=&JobsFilter%5Bcategory%5D%5B%5D=1&JobsFilter%5Bjob_type%5D=&JobsFilter%5Bjob_term%5D=&JobsFilter%5Bjob_city%5D=&JobsFilter%5Bjob_package%5D=&JobsFilter%5Bsort_by%5D=";
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load(url);
-            string job = "//div[@class=\"job-inner job-item-title\"]";
-           
-            string job2 = "//div[@class='job-inner job-list-deadline']";
-            HtmlNodeCollection jobItemTitle = doc.DocumentNode.SelectNodes(job);
-            HtmlNodeCollection jobԼistDeadline = doc.DocumentNode.SelectNodes(job2);
-            List<ActiveJobs> activeJobs = new List<ActiveJobs>();
-            for (int i = 0; i < jobItemTitle.Count; i++)
-            {
-                string[] splitText = jobItemTitle[i].InnerText.Split('\n');
-                string[] splitText2 = jobԼistDeadline[i].InnerText.Split('\n');
-                string temp = splitText2[3] + splitText2[4] + splitText2[5];
-                activeJobs.Add(new ActiveJobs { JobName = splitText[1], CompanyName = splitText[2],Data=temp});
-            }
+       
 
-            foreach (var item in activeJobs)
+        public static void WriteExceptionInFile(Exception e)
+        {
+            FileStream fileStream = new FileStream("exception.txt", FileMode.Append);
+            using (StreamWriter writer = new StreamWriter(fileStream))
             {
-                Console.WriteLine($"Company:{item.CompanyName},Data:{item.Data},Job:{item.JobName}");
-            }
-            Console.ReadLine();
+                writer.WriteLine(e);
+                writer.Flush();
+            }            
         }
-        static void Main(string[] args)
+
+        internal static void WriteExceptionInFile(string message)
         {
-            //SearchActiveJob();
-            string url = @"https://staff.am/en/companies?CompaniesFilter%5BkeyWord%5D=&CompaniesFilter%5Bindustries%5D=&CompaniesFilter%5Bindustries%5D%5B%5D=2&CompaniesFilter%5Bemployees_number%5D=&CompaniesFilter%5Bsort_by%5D=&CompaniesFilter%5Bhas_job%5D=";
-            Helper.ScrapForStaffAM(url);
+            throw new NotImplementedException();
+        }
 
-            Console.ReadKey();
-            HtmlWeb web = new HtmlWeb();
+        static void Main(string[] args)
+        {           
+            string urlForSearchAllCompanies = @"https://staff.am/en/companies?CompaniesFilter%5BkeyWord%5D=&CompaniesFilter%5Bindustries%5D=&CompaniesFilter%5Bindustries%5D%5B%5D=2&CompaniesFilter%5Bemployees_number%5D=&CompaniesFilter%5Bsort_by%5D=&CompaniesFilter%5Bhas_job%5D=";    
+            Helper.ScrapForStaffAM(urlForSearchAllCompanies);
 
-            string directory = @"C:\Users\suren\source\repos\HTMLScrapping\HTMLScrapping\bin\Debug\netcoreapp2.1";
-            ChromeOptions co = new ChromeOptions();
-            co.AddArgument("--disable-images");
-            ChromeDriver chromeDriver = new ChromeDriver(directory, co);
-
-            chromeDriver.Navigate().GoToUrl(url);
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    try
-            //    {
-            //        chromeDriver.ExecuteScript($"window.scrollBy(0,1750);");
-            //    }
-            //    catch (Exception e)
-            //    {
-
-            //    }
-            //    Thread.Sleep(1000);
-            //}
-
-            HtmlDocument doc = new HtmlDocument();
-            doc.LoadHtml(chromeDriver.PageSource);
+            string urlForSearchAllActiveJobs = "https://staff.am/en/jobs?JobsFilter%5Bkey_word%5D=&JobsFilter%5Bcategory%5D=&JobsFilter%5Bcategory%5D%5B%5D=1&JobsFilter%5Bjob_type%5D=&JobsFilter%5Bjob_term%5D=&JobsFilter%5Bjob_city%5D=&JobsFilter%5Bjob_package%5D=&JobsFilter%5Bsort_by%5D=";
+            Helper.SearchActiveJob(urlForSearchAllActiveJobs);
+    
+           
 
            
         }
